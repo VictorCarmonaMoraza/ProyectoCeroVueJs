fab637169fdcdfd2f77da8dab175e05c7e409818
Victor <VictorCarmona9035@hotmail.com>
2023-09-22 08:31:13 +0200

    Representacion declarativa (32)

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const app = Vue.createApp({
-    template:`
-    <h1>Hola Mundo</h1>
-    <p>Desde app.js</p>
-    `
+    // template:`
+    // <h1>Hola Mundo</h1>
+    // <p>{{ 1 + 1}}</p>
+    // <p>{{ '1' + '1'}}</p>
+    // <p>{{ true}}</p>
+    // <p>{{ [1,2,3,4,5,6]}}</p>
+    // <p>{{ {a:1,b:2} }}</p>
+    // <p>{{ [ {}, {}, {}, [] ] }}</p>
+    // <p>{{ true ? 'Activo':'Inactivo' }}</p>
+    // <p>Desde app.js</p>
+    // `
 })
 
 app.mount('#myApp')
